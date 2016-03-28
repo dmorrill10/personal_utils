64d0eb9a39abb134cdb99ea714abc4c7ac25bc89
Dustin Morrill <dmorrill10@gmail.com>
2016-03-28 10:15:57 -0600

    Add methods to save and load vector--value map

--- python/personal_utils/sequence_with_value_serializer.py
+++ python/personal_utils/sequence_with_value_serializer.py
@@ -1,10 +1,20 @@
 import struct
 from builtins import bytes
+import array
+
+
+def vector_to_pvector(v):
+    return bytes([len(v)]) + bytes(v)
+
+
+def read_vector_from_pvector(read_bytes, number_format='d'):
+    length = read_bytes()[0]
+    return array.array(number_format, read_bytes(length))
 
 
 def string_to_pstring(s, encoding='ascii'):
     bstring = s.encode(encoding)
-    return bytes(chr(len(bstring)).encode(encoding) + bstring)
+    return bytes([len(bstring)]) + bytes(bstring)
 
 
 def read_string_from_pstring(read_bytes, encoding='ascii'):
@@ -13,13 +23,56 @@ def read_string_from_pstring(read_bytes, encoding='ascii'):
 
 
 class Serializer():
-    def __init__(self, encoding='ascii'):
+    STRING_KEY_FORMAT = 's'
+
+    def __init__(self, encoding='ascii', key_format=STRING_KEY_FORMAT):
         self.value_packer = struct.Struct('d')
         self.encoding = encoding
+        self.key_format = key_format
 
-    def save(self, sequences_with_values, file_name):
+    def save(self, key_value_map, file_name):
+        if self.key_format == self.STRING_KEY_FORMAT:
+            return self._save_string_value_map(key_value_map, file_name)
+        else:
+            return self._save_vector_value_map(key_value_map, file_name)
+
+    def load(self, file_name):
+        if self.key_format == self.STRING_KEY_FORMAT:
+            return self._load_string_value_map(file_name)
+        else:
+            return self._load_vector_value_map(file_name)
+
+    def _save_vector_value_map(self, vector_to_value_map, file_name):
         data = bytes()
-        for sequence, value in sequences_with_values.items():
+        for sequence, value in vector_to_value_map.items():
+            data += (
+                vector_to_pvector(sequence) +
+                self.value_packer.pack(value)
+            )
+        with open(file_name + ".swv", 'wb') as f:
+            f.write(data)
+
+    def _load_vector_value_map(self, file_name):
+        vector_to_value_map = {}
+        with open(file_name + ".swv", 'rb') as f:
+            while True:
+                try:
+                    sequence = read_vector_from_pstring(
+                        lambda n=1: f.read(n),
+                        number_format=self.number_format
+                    )
+                    vector_to_value_map[
+                        sequence
+                    ] = self.value_packer.unpack(
+                        f.read(self.value_packer.size)
+                    )[0]
+                except IndexError:
+                    break
+        return string_to_value_map
+
+    def _save_string_value_map(self, string_to_value_map, file_name):
+        data = bytes()
+        for sequence, value in string_to_value_map.items():
             data += (
                 string_to_pstring(sequence, self.encoding) +
                 self.value_packer.pack(value)
@@ -27,8 +80,8 @@ class Serializer():
         with open(file_name + ".swv", 'wb') as f:
             f.write(data)
 
-    def load(self, file_name):
-        sequences_with_values = {}
+    def _load_string_value_map(self, file_name):
+        string_to_value_map = {}
         with open(file_name + ".swv", 'rb') as f:
             while True:
                 try:
@@ -36,11 +89,11 @@ class Serializer():
                         lambda n=1: f.read(n),
                         self.encoding
                     )
-                    sequences_with_values[
+                    string_to_value_map[
                         sequence
                     ] = self.value_packer.unpack(
                         f.read(self.value_packer.size)
                     )[0]
                 except IndexError:
                     break
-        return sequences_with_values
+        return string_to_value_map
