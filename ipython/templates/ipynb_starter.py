13147f608a3b0cdeab6c8e9f2477574be7a9474b
Dustin Morrill <dmorrill10@gmail.com>
2017-11-26 17:03:26 -0700

    Add simple iPython experiment template generator.

---
+++ ipython/templates/ipynb_starter.py
@@ -0,0 +1,10 @@
+# Automatic module reloading
+%load_ext autoreload
+%autoreload 2
+
+# Basic plotting
+import matplotlib.pyplot as plt
+
+# Interactive plotting
+import mpld3
+mpld3.enable_notebook()
