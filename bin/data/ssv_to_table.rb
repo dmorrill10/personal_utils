37a9924ecd73bb640105f5fe9fd04524e62a8680
Dustin Morrill <dmorrill10@gmail.com>
2020-08-10 20:22:23 -0600

    Improve script.

--- bin/data/ssv_to_table.rb
+++ bin/data/ssv_to_table.rb
@@ -60,6 +60,43 @@ def parse_mean_data
   num_columns
 end
 
+def parse_table
+  line = STDIN.gets.strip
+  while line.match(/^\s*$/)
+    line = STDIN.gets.strip
+  end
+  column_labels = line.split(/\s+/) + ['avg']
+  $element_width = column_labels.inject(0) do |max_len, label|
+    [label.length, max_len].max
+  end + 4
+  column_labels[0..-2].each do |label|
+    printf("%#{$element_width}s", format("%s", label))
+  end
+  printf("%#{$element_width}s", format("%s", column_labels[-1]))
+  puts ""
+  column_labels.each do
+    printf(" %#{$element_width-1}s", '-'*($element_width-1))
+  end
+  puts ""
+  col_avgs = (column_labels.length - 1).times.map { 0.0 }
+  row_avgs = []
+  num_rows = 0
+  STDIN.each do |line|
+    line.strip!
+    row_data = line.split(/\s+/)
+    num_rows += 1
+    row_avg = row_data.inject(0.0) do |avg, v|
+      avg + v.to_f / (column_labels.length - 1.0)
+    end
+    (row_data[1..-1] + [row_avg]).each_with_index do |value, i|
+      col_avgs[i] += (value.to_f - col_avgs[i]) / num_rows.to_f
+    end
+    yield row_data[0], row_data[1..-1], row_avg
+  end
+  yield 'avg', col_avgs[0..-2], col_avgs[-1]
+  column_labels.length
+end
+
 def print_table_row(row_label, means, plus_minus, num_digits_after_decimal)
   printf("%#{$element_width}s", format("`%s`", row_label))
   means.each_with_index do |mean, index|
@@ -67,6 +104,25 @@ def print_table_row(row_label, means, plus_minus, num_digits_after_decimal)
 
     printf("%#{$element_width}s", format("$%.#{num_digits_after_decimal}f \\pm %.#{num_digits_after_decimal}f$", mean, plus_minus[index]))
   end
+  puts ""
+end
+
+def float_to_tex(value, num_digits_after_decimal, scale)
+  v = value.to_f * scale
+  v = 0 if v.abs < 1e-15
+  format("$%.#{num_digits_after_decimal}f$", v)
+end
+
+def print_table_row_no_pm(row_label, values, avg, num_digits_after_decimal, scale)
+  printf("%#{$element_width}s", format("%s", row_label))
+  values.each_with_index do |value, index|
+    printf(
+      "%#{$element_width}s",
+      float_to_tex(value, num_digits_after_decimal, scale))
+  end
+  printf(
+    "%#{$element_width}s",
+    float_to_tex(avg, num_digits_after_decimal, scale))
   puts ""
 end
 
@@ -83,16 +139,38 @@ OptionParser.new do |opts|
 
   options[:digits] = 2
   opts.on("-d", "--digits [NUMBER OF DIGITS]", "Number of digits after the decimal to print") do |d|
-    options[:digits] = d.to_i || 2
+    options[:digits] = d.to_i || options[:digits]
+  end
+
+  options[:from_ssv_table] = false
+  opts.on("--from-ssv-table", "Convert an SSV table to a pandoc table.") do |arg|
+    options[:from_ssv_table] = arg unless arg.nil?
+  end
+
+  options[:scale] = 1.0
+  opts.on("--scale [SCALE]", "A scaling factor with which to adjust all values in the table.") do |arg|
+    options[:scale] = arg.to_f unless arg.nil?
   end
 end.parse!
 
-num_columns = parse_mean_data do |row_label, means, plus_minus|
-  print_table_row(row_label, means, plus_minus, options[:digits])
-end
-num_columns.times.each do |index|
-  next unless index % 10 == 0
+if options[:from_ssv_table]
+  num_columns = parse_table do |row_label, values, avg|
+    print_table_row_no_pm(row_label, values, avg, options[:digits], options[:scale])
+  end
+  (num_columns - 1).times.each do |index|
+    # next unless index % 10 == 0
+    printf(" %#{$element_width-1}s", '-'*($element_width-1))
+  end
+  printf(" %#{$element_width - 1}s", '-'*($element_width - 1))
+  puts ""
+else
+  num_columns = parse_mean_data do |row_label, means, plus_minus|
+    print_table_row(row_label, means, plus_minus, options[:digits])
+  end
+  num_columns.times.each do |index|
+    next unless index % 10 == 0
+    printf(" %#{$element_width-1}s", '-'*($element_width-1))
+  end
   printf(" %#{$element_width-1}s", '-'*($element_width-1))
+  puts ""
 end
-printf(" %#{$element_width-1}s", '-'*($element_width-1))
-puts ""
